35d51c1e7e6004a25ff4ae161d35aff2360f24d5
Thealoq <79800238+Thealoq@users.noreply.github.com>
2022-02-11 17:26:13 +0300

    Update Card.vue

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -44,7 +44,7 @@ export default {
             message: item.displayName,
             img: item.Image,
             description: item.description,
-            link: "https://valorantwiki.netlify.app/agents/" + item.uuid,
+            link: "valorantwiki.vercel.app/agents/" + item.uuid,
 
           });
         });
