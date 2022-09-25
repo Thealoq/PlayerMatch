94b007469f59af1134a5d1f6e85f67f99eccd8ca
Thealoq <79800238+Thealoq@users.noreply.github.com>
2022-09-25 15:53:31 +0300

    responsive
    comp home 20%

--- src/Page/Home.jsx
+++ src/Page/Home.jsx
@@ -1,8 +1,22 @@
 import { useState,useEffect } from "react";
+import Camp from "../assets/Camp.png"
+
+
+import "../app.css"
 export default function Home() {
     return (
         <div className="center">
-            
+            <div className="agent-cards-body">
+                <div className="agent-home-title">
+                 <div>
+                 <h3>Valorant: The 5v5 Competitive Game</h3>
+                 <p>A 5v5 tactical FPS with outstanding characters, precise shooting mechanics and unique skills!</p></div> 
+                </div>
+                <div className="center">
+                    <div><img src={Camp} alt="Camp" /></div>
+                </div>
+              
+            </div>
         </div>
     )
 }
