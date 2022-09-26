d2f9a68ceb425d60600888189910c6b4f29742a3
Thealoq <79800238+Thealoq@users.noreply.github.com>
2022-09-26 16:57:06 +0300

    content page
    :anchor:

--- src/Page/AgentContent.jsx
+++ src/Page/AgentContent.jsx
@@ -1,8 +1,36 @@
 import { useState, useEffect } from "react";
+import { useNavigate, useParams } from 'react-router-dom';
+
 export default function AgentContent() {
+    const { name } = useParams()
+    const [Content, SetArray] = useState([]);
+    useEffect(() => {
+        fetch("https://valorant-api.com/v1/agents/" + name)
+            .then(response => response.json())
+            .then(json => {
+                SetArray(json.data)
+            });
+    }, []);
     return (
         <div className="center">
-           
+            <div className="agentcontent-cards-bodys">
+                <div style={{
+                backgroundImage: "url(" + Content.background + ")",
+                backgroundPosition: 'center',
+                backgroundSize: 'cover',
+                backgroundRepeat: 'no-repeat'
+            }}  className="agentcontent-img">
+                    <img className="center" src={Content.fullPortrait} alt={Content.displayName} />
+                </div>
+                <div className="agentcontent-content">
+                    <div><h3>{Content.displayName}</h3></div>
+                    <div><p>{Content.description}</p></div>
+                    <div><h3>{Content.role?.displayName}</h3></div>
+                    <div><p>{Content.role?.description}</p></div>
+                </div>
+
+            </div>
         </div>
     )
+
 }
--- src/Page/Agents.jsx
+++ src/Page/Agents.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from "react";
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
 export default function Agents() {
-    const navigateTo = useNavigate();
+    const navigateTo = useNavigate()
       const [Content, SetArray] = useState([]);
       function Fetch() {
         fetch("https://valorant-api.com/v1/agents")
@@ -15,7 +15,7 @@ export default function Agents() {
     }, []);
 
     function AgentsPage(e) {
-        const link = window.location.origin + "/Agents" + "/" + e
+        const link = "/Agents" + "/" + e
         navigateTo(link, { replace: true })
     }
     
@@ -26,7 +26,12 @@ export default function Agents() {
                 {
                     Content.map(( item, index ) => {
                         return(
-                            <div onClick={event => AgentsPage(item.uuid)} key={index} className="agent-card-witdh">
+                            <div style={{
+                                backgroundImage: "url(" + item.background + ")",
+                                backgroundPosition: 'center',
+                                backgroundSize: 'cover',
+                                backgroundRepeat: 'no-repeat'
+                            }}  onClick={event => AgentsPage(item.uuid)} key={index} className="agent-card-witdh">
                                <div>
                                     <h1>{item.displayName}</h1>
                                 </div>
