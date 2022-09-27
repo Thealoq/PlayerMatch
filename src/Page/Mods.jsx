5503c1a26c9aec68c1002be555adc8452c6b1388
Thealoq <79800238+Thealoq@users.noreply.github.com>
2022-09-27 16:30:34 +0300

    oh nice
    :+1:

--- src/Page/Mods.jsx
+++ src/Page/Mods.jsx
@@ -11,25 +11,35 @@ export default function Mods() {
             });
     }, []);
 
-     console.log(Mods);
     function ModPage(e) {
         const link = "/Mods" + "/" + e
         navigateTo(link, { replace: true })
     }
 
     return (
-            <div className="allmods-body">
-                {
-                    Mods.map((item, index) => {
-                        return (
-                            <div key={index} className="map-body">
-                                <div onClick={event => ModPage(item.uuid)} className="map-bg center">
-                                    <h2>{item.displayName}</h2>
-                                </div>
+        <div className="allmods-body">
+            {
+                Mods.map((item, index) => {
+                    if (!item.displayIcon) return <div onClick={event => ModPage(item.uuid)} key={index} className="map-body withmods">
+                            <div className="map-bg">
+                                <div><img width={128} height={128} src="https://cdn.discordapp.com/attachments/902302627767918602/1023996010629963890/pngegg_2.png" /></div>
                             </div>
-                        )
-                    })
-                }
-            </div>
+                            <div className="map-bg">
+                                <div><h2>{item.displayName}</h2></div>
+                            </div>
+                        </div>
+                    return (
+                        <div onClick={event => ModPage(item.uuid)} key={index} className="map-body withmods">
+                            <div className="map-bg">
+                                <div><img src={item.displayIcon} /></div>
+                            </div>
+                            <div className="map-bg">
+                                <div><h2>{item.displayName}</h2></div>
+                            </div>
+                        </div>
+                    )
+                })
+            }
+        </div>
     )
 }
