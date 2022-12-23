723b9ad3b0c1195d28449546573ad430c669e2eb
Thealoq <79800238+Thealoq@users.noreply.github.com>
2022-12-23 03:24:14 +0300

    bad pages
    👎

--- src/views/Agents.jsx
+++ src/views/Agents.jsx
@@ -1,5 +1,5 @@
-import { useState,useEffect } from "react";
-import { useNavigate } from 'react-router-dom';               
+import { useState, useEffect } from "react";
+import { useNavigate } from 'react-router-dom';
 export default function Agents() {
     const navigateTo = useNavigate()
     const [Content, SetArray] = useState([]);
@@ -20,17 +20,45 @@ export default function Agents() {
 
     return (
         <div>
-            {
-                Content?.map(t => {
-                    return (
-                        <div className="flex justify-center items-center" onClick={e => AgentsPage(t.uuid)}>
-                            <div>
-                                <img width={400} height={400} src={t.fullPortrait} alt={t.displayName} />
-                            </div>
-                        </div>
-                    )
-                })
-            }
+            <div className="p-20">
+                <div className="flex justify-center items-center text-left text-2xl text-bold m-2 ">
+                    <span>POPULAR AGENTS</span>
+                </div>
+                <div className="flex flex-wrap w-full justify-center items-center">
+                    {
+                        Content?.map((t, i) => {
+                            return (
+                                <div key={i} style={{
+                                    backgroundPosition: "center 40%",
+                                    backgroundRepeat: "no-repeat",
+                                    backgroundImage: `url(https://media.valorant-api.com/agents/${t.uuid}/background.png)`,
+                                }} className="flex justify-center w-[320px] m-2 items-center flex-col rounded-md" onClick={e => AgentsPage(t.uuid)}>
+                                    <div className="w-full h-[150px] object-cover rounded-t-md" style={{
+                                        backgroundPosition: "center 20%",
+                                        backgroundRepeat: "no-repeat",
+                                        backgroundImage: `url(https://media.valorant-api.com/agents/${t.uuid}/fullportrait.png)`,
+                                    }}></div>
+                                    <div className="border-4 mt-[-1rem]  bg-[#1c1a1f] absolute border-[#1c1a1f] rounded-full flex justify-start items-center">
+                                        <img className="rounded-full" width={60} height={60} src={`https://media.valorant-api.com/agents/${t.uuid}/displayicon.png`} />
+                                    </div>
+                                    <div className="flex-col flex justify-center w-full bg-[#1c1a1f] h-[160px]">
+                                        <div className="mb-4 ml-[2.3rem]">
+                                            <div className="mt-[1rem] ml-2 flex justify-start items-center text-bold">
+                                                {t.displayName}
+                                            </div>
+                                            <div className="mt-[0.2rem] ml-2 flex justify-start items-center text-xs">
+                                                <div className="w-[92%]">
+                                                    {t.description}
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            )
+                        })
+                    }
+                </div>
+            </div>
         </div>
     )
 }
--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,7 +1,11 @@
-export default function Home () {
-    return(
+export default function Home() {
+    return (
         <div>
-            Home Page
+            <div class="relative w-full px-4 py-16 lg:py-32 bg-primary-400 text-center bg-[#FFFF3C] selection:text-primary-400">
+
+                qwe
+
+            </div>
         </div>
     )
 }
