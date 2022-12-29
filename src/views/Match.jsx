cdadef030725ad9ebff6df0205eb8cc985bd7b8d
Thealoq <79800238+Thealoq@users.noreply.github.com>
2022-12-29 05:37:04 +0300

    ☕

--- src/views/Match.jsx
+++ src/views/Match.jsx
@@ -1,5 +1,6 @@
 import banner from "../Assets/match.jpg"
-import img from "../Assets/av.jpg"
+import MatchPic from "../Assets/match/macth.jpeg"
+import MatchPicTwo from "../Assets/match/match2.jpg"
 export default function Match() {
     return (
         <div>
@@ -12,23 +13,39 @@ export default function Match() {
                 <h1 className="text-3xl md:text-4xl  font-bold uppercase text-white">Match Player</h1>
                 <h2 className="text-white mb-8 mt-4 font-medium lg:text-lg ">Let's match the most suitable player for you together</h2>
             </div>
-            <div className="flex justify-center items-center mt-[4rem]">
-                <div className="w-[120px] h-[360px] bg-[#1C1A1F] m-2 mr-[22rem] mt-4 absolute -rotate-12 z-0 flex justify-center items-center">
-                    <span>Önceki</span>
-                </div>
-                <div className="w-[300px] h-[400px] bg-[#3b3741] m-2 z-10 flex justify-center items-center flex-col">
-                    <div className="m-2">
-                        <img className="rounded-full" src={img} />
-                    </div>
-                    <div className="m-2">
-                        <span>Thealoq</span>
-                    </div>
-                    <div>
-
+            <div className="flex justify-center items-center flex-wrap mt-10 ">
+                <div className="rotate-12 z-10 mr-[-4rem] opacity-50">
+                    <div className=" flex justify-center items-center w-[200px]  h-[270px] rounded-lg z-10 " style={{
+                        backgroundPosition: "center 50%",
+                        backgroundRepeat: "no-repeat",
+                        backgroundImage: `url(${MatchPic})`,
+                        backgroundSize: "cover",
+                    }}> </div>
+                    <div className="w-[200px] h-[150px] bg-[#1C1A1F] z-20 mt-[-4rem] flex justify-center items-center rounded-t-lg">
+                        Thealoq
                     </div>
                 </div>
-                <div className="w-[150px] h-[360px] bg-[#1C1A1F] m-2 mt-4 ml-[22rem] absolute rotate-12 z-0 flex justify-center items-center">
-                    <span>Sıradaki</span>
+                <div className="z-20">
+                    <div className=" flex justify-center items-center w-[200px]  h-[300px] rounded-lg z-10 " style={{
+                        backgroundPosition: "center 50%",
+                        backgroundRepeat: "no-repeat",
+                        backgroundImage: `url(${banner})`,
+                        backgroundSize: "cover",
+                    }}> </div>
+                    <div className="w-[200px] h-[150px] bg-[#1C1A1F] z-20 mt-[-4rem] flex justify-center items-center rounded-t-lg">
+                        Thealoq
+                    </div>
+                </div>
+                <div className="-rotate-12 z-10 ml-[-4rem] opacity-50" >
+                    <div className=" flex justify-center items-center w-[200px]  h-[270px] rounded-lg z-10 " style={{
+                        backgroundPosition: "center 50%",
+                        backgroundRepeat: "no-repeat",
+                        backgroundImage: `url(${MatchPicTwo })`,
+                        backgroundSize: "cover",
+                    }}> </div>
+                    <div className="w-[200px] h-[150px] bg-[#1C1A1F] z-20 mt-[-4rem] flex justify-center items-center rounded-t-lg">
+                        Thealoq
+                    </div>
                 </div>
             </div>
         </div>
